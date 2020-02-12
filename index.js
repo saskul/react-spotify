c3ca2b08eedbb567d2294ee2872035c64ea7839d
otherutterotter <filipsaskulczycki@gmail.com>
2020-02-12 11:06:02 +0100

    develop env authentication

--- index.js
+++ index.js
@@ -5,23 +5,35 @@ const axios = require("axios");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-
 const {
 	  PORT,
+	  REACT_APP_PORT,
 	  SPOTIFY_WEB_API,
 	  SPOTIFY_CLIENT_ID,
 	  SPOTIFY_SECRET,
 	  SPOTIFY_REDIRECT_URI,
+	  SPOTIFY_REDIRECT_URI_DEV,
 	  SPOTIFY_SCOPES
 } = process.env;
 
 app.use(cors());
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
+app.get('/authorize/develop', (req, res) => {
+	  res.redirect(
+	    `${SPOTIFY_WEB_API}/authorize?client_id=${SPOTIFY_CLIENT_ID}&redirect_uri=${SPOTIFY_REDIRECT_URI_DEV}&response_type=code`
+	  );
+});
+
 app.get('/authorize', (req, res) => {
-	  res.redirect(`${SPOTIFY_WEB_API}/authorize?client_id=${SPOTIFY_CLIENT_ID}&redirect_uri=${SPOTIFY_REDIRECT_URI}&response_type=code`)
+	  res.redirect(
+	    `${SPOTIFY_WEB_API}/authorize?client_id=${SPOTIFY_CLIENT_ID}&redirect_uri=${SPOTIFY_REDIRECT_URI}&response_type=code`
+	  );
+});
+
+app.get('/develop', (req, res) => {
+    res.redirect(`http://localhost:${REACT_APP_PORT}?code=${req.query.code}`);
 });
 
 app.get('/', (req, res) => {
