b1e046b40999e11521df2d3cb3fb3a01cbe43f73
root <filipsaskulczycki@gmail.com>
2020-02-12 09:43:03 +0000

    connect server to API in production

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const createURI = require("./src/helpers/createURI");
+const axios = require("axios");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 
 const {
-  PORT,
-  SPOTIFY_WEB_API,
-  SPOTIFY_CLIENT_ID,
-  SPOTIFY_SECRET,
-  SPOTIFY_REDIRECT_URI,
-  SPOTIFY_SCOPES
+	  PORT,
+	  SPOTIFY_WEB_API,
+	  SPOTIFY_CLIENT_ID,
+	  SPOTIFY_SECRET,
+	  SPOTIFY_REDIRECT_URI,
+	  SPOTIFY_SCOPES
 } = process.env;
 
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
 app.get('/authorize', (req, res) => {
-  const uri = createURI(SPOTIFY_WEB_API, {
-    client_id: SPOTIFY_CLIENT_ID,
-    response_type: 'code',
-    redirect_uri: SPOTIFY_REDIRECT_URI,
-    // state: 'TO-DO: set cookie',
-    scope: SPOTIFY_SCOPES
-  });
-  fetch(uri)
-  .then(result => result.json())
-  .then(result => res.json(result))
-  .catch(err => res.status(404).json({ error: err }))
+	  res.redirect(`${SPOTIFY_WEB_API}/authorize?client_id=${SPOTIFY_CLIENT_ID}&redirect_uri=${SPOTIFY_REDIRECT_URI}&response_type=code`)
 });
 
 app.get('/', (req, res) => {
-  res.send("Hello, world!");
+	  res.send("Hello, world!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+	  console.log(`Server is listening on port ${PORT}`);
 });
