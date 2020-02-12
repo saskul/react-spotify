796f5e3c0501ee46d7cf779daf9ec71a556f6925
root <filipsaskulczycki@gmail.com>
2020-02-12 10:59:37 +0000

    fix controllers

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const axios = require('axios');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 const authController = require('./controllers/auth');
 
 const { HOST_PORT, PORT } = process.env;
