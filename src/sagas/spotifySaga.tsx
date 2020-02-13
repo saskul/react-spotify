80773b6e06357d10e1407820d6cd0e5c81a4a33c
otherutterotter <filipsaskulczycki@gmail.com>
2020-02-13 07:15:01 +0100

    refresh token

--- src/sagas/spotifySaga.tsx
+++ src/sagas/spotifySaga.tsx
@@ -2,7 +2,8 @@ import { put, takeLatest } from 'redux-saga/effects';
 import {
   GET_USER_PLAYLISTS,
   SET_USER_PLAYLISTS,
-  SPOTIFY_FAILURE
+  SPOTIFY_FAILURE,
+  REFRESH_TOKEN
 } from '../types';
 import { spotifyService } from '../services';
 
@@ -10,6 +11,7 @@ export function* fetchUserPlaylists({ type, token }: any) {
   try {
     const playlists = yield spotifyService.getUserPlaylists(token);
     yield put({ type: SET_USER_PLAYLISTS, playlists });
+    yield put({ type: REFRESH_TOKEN, refresh_token: token['refresh_token'] });
   } catch (error) {
     console.error(error)
     yield put({ type: SPOTIFY_FAILURE, error });
