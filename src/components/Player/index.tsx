f7afd6086f3581d96cd8a97636dd0f93a820c8cd
otherutterotter <filipsaskulczycki@gmail.com>
2020-02-15 00:45:11 +0100

    add distant cough

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,13 +1,128 @@
 import React from 'react';
+import distant_cough from '../../static/distant_cough.mp3';
 import './Player.scss';
 
 type Props = {};
 type State = {};
 
-class Player extends React.Component<Props, State> {
-  render() {
+
+const campfireStory = distant_cough;
+const bootingUp = distant_cough;
+
+function getTime(time) {
+  if (!isNaN(time)) {
     return (
-      <div className="player --underlight-font">Player</div>
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
+  }
+}
+
+class Player extends React.Component<Props, State> {
+  state = {
+    selectedTrack: null,
+    player: "stopped",
+    currentTime: null,
+    duration: null
+  };
+
+  componentDidMount() {
+    this.player.addEventListener("timeupdate", e => {
+      this.setState({
+        currentTime: e.target.currentTime,
+        duration: e.target.duration
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.player.removeEventListener("timeupdate", () => {});
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.selectedTrack !== prevState.selectedTrack) {
+      let track;
+      switch (this.state.selectedTrack) {
+        case "Campfire Story":
+          track = campfireStory;
+          break;
+        case "Booting Up":
+          track = bootingUp;
+          break;
+        default:
+          break;
+      }
+      if (track) {
+        this.player.src = track;
+        this.player.play();
+        this.setState({ player: "playing", duration: this.player.duration });
+      }
+    }
+    if (this.state.player !== prevState.player) {
+      if (this.state.player === "paused") {
+        this.player.pause();
+      } else if (this.state.player === "stopped") {
+        this.player.pause();
+        this.player.currentTime = 0;
+        this.setState({ selectedTrack: null });
+      } else if (
+        this.state.player === "playing" &&
+        prevState.player === "paused"
+      ) {
+        this.player.play();
+      }
+    }
+  }
+
+  render() {
+    const list = [
+      { id: 1, title: "Campfire Story" },
+      { id: 2, title: "Booting Up" }
+    ].map(item => {
+      return (
+        <li
+          key={item.id}
+          onClick={() => this.setState({ selectedTrack: item.title })}
+        >
+          {item.title}
+        </li>
+      );
+    });
+
+    const currentTime = getTime(this.state.currentTime);
+    const duration = getTime(this.state.duration);
+
+    return (
+      <>
+        <h1>My Little Player</h1>
+        <ul>{list}</ul>
+        <div>
+          {this.state.player === "paused" && (
+            <button onClick={() => this.setState({ player: "playing" })}>
+              Play
+            </button>
+          )}
+          {this.state.player === "playing" && (
+            <button onClick={() => this.setState({ player: "paused" })}>
+              Pause
+            </button>
+          )}
+          {this.state.player === "playing" || this.state.player === "paused" ? (
+            <button onClick={() => this.setState({ player: "stopped" })}>
+              Stop
+            </button>
+          ) : (
+            ""
+          )}
+        </div>
+        {this.state.player === "playing" || this.state.player === "paused" ? (
+          <div>
+            {currentTime} / {duration}
+          </div>
+        ) : (
+          ""
+        )}
+        <audio ref={ref => (this.player = ref)} />
+      </>
     );
   }
 }
